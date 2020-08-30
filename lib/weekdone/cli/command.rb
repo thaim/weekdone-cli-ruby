ed46819df932d0d5dd065cbdd758ddba1ca6a6b1
thaim <thaim24@gmail.com>
2020-08-30 23:35:48 +0000

    implement commands without subcommand

--- lib/weekdone/cli/command.rb
+++ lib/weekdone/cli/command.rb
@@ -8,13 +8,15 @@ require_relative 'command_company'
 
 module Weekdone::Cli
   class Command < Thor
+    include Weekdone::Cli
+
     def self.exit_on_failure?
       true
     end
 
     desc "login", "oauth login to weekdone"
     def login
-      client = build
+      client = build_client
 
       puts "open URL to authorize:"
       puts client.authorization_request
@@ -25,6 +27,43 @@ module Weekdone::Cli
 
       Credential.write_credential(client.token_hash)
     end
+
+    desc "report", "get report"
+    def report
+      client = build_client
+
+      report = client.getReport
+      Credential.write_credential(client.token_hash)
+      Output.format(report, format: :json)
+    end
+
+    desc "teams", "get all teams"
+    def teams
+      client = build_client
+
+      teams = client.getAllTeams
+      Credential.write_credential(client.token_hash)
+      Output.format(teams, format: :json)
+    end
+
+    desc "users", "get all users"
+    def users
+      client = build_client
+
+      users = client.getAllUsers
+      Credential.write_credential(client.token_hash)
+      Output.format(users, format: :json)
+    end
+
+    desc "types", "get all types"
+    def types
+      client = build_client
+
+      types = client.getAllTypes
+      Credential.write_credential(client.token_hash)
+      Output.format(types, format: :json)
+    end
+
 
     register(Item, 'item', 'item [COMMAND]', 'command for item')
     register(Tag, 'tag', 'tag [COMMAND]', 'command for tag')
